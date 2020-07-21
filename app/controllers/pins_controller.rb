4f0c61fb4a0bf4092e42528dbef1509e224262f4
serenanamee <serena.chang169@gmail.com>
2020-07-21 17:12:49 +0800

    second commit

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,6 +1,36 @@
 class PinsController < ApplicationController
-  def index
 
+  before_action :find_pin, only: [:show, :edit, :update, :destroy]
+
+ def index
+  @pins = Pin.all.order("created_at DESC")
+end
+
+  def new
+    @pin = Pin.new
+  end
+
+  def create
+    @pin = Pin.new(pin_params)
+    if @pin.save
+      redirect_to @pin, notice: "successfully created new Pin"
+    else
+       render 'new'
+    end
+  end
+
+  def show
+
+  end
+
+  private
+
+  def pin_params
+    params.require(:pin).permit(:title, :description)
+  end
+
+  def find_pin
+    @pin = Pin.find(params[:id])
   end
 
 end
